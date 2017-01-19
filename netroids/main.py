22757f20d0d2c79959967983207f816024fe3f32
Garrison Benson <benson.garrison@gmail.com>
2017-01-18 20:53:39 -0500

    Renamed variables/functions for PEP8.

--- netroids/main.py
+++ netroids/main.py
@@ -1,28 +1,29 @@
 import socket
 
 
-def getLocalAddress():
-    localAddress = socket.gethostbyname(socket.gethostname())
-    typedAddress = raw_input(
-        "Enter your external IP address (default: "+localAddress+"): ").strip()
-    if typedAddress != "":
-        localAddress = typedAddress
-    return localAddress
+def get_local_address():
+    local_address = socket.gethostbyname(socket.gethostname())
+    typed_address = raw_input(
+        "Enter your external IP address (default: "+local_address+"): ").strip()
+    if typed_address != "":
+        local_address = typed_address
+    return local_address
 
-name = raw_input("Enter your name (default: Player): ").strip()
-name = name.replace(" ", "").replace("|", "").replace(":", "")
-if name == "":
-    name = "Player"
-clientOrServer = raw_input(
-    "Type 1 to start a server, or 2 to start as a client (default: 1): ")
-if clientOrServer.strip() == "2":
-    localAddress = getLocalAddress()
-    serverAddress = raw_input("Enter the IP address of a server: ").strip()
-    import mClient
-    client = mClient.Client(localAddress, serverAddress, name)
-    client.go()
-else:
-    localAddress = getLocalAddress()
-    import mServer
-    server = mServer.Server(localAddress, name)
-    server.go()
+if __name__ == "__main__":
+    name = raw_input("Enter your name (default: Player): ").strip()
+    name = name.replace(" ", "").replace("|", "").replace(":", "")
+    if name == "":
+        name = "Player"
+    client_or_server = raw_input(
+        "Type 1 to start a server, or 2 to start as a client (default: 1): ")
+    if client_or_server.strip() == "2":
+        local_address = get_local_address()
+        server_address = raw_input("Enter the IP address of a server: ").strip()
+        import mClient
+        client = mClient.Client(local_address, server_address, name)
+        client.go()
+    else:
+        local_address = get_local_address()
+        import mServer
+        server = mServer.Server(local_address, name)
+        server.go()
